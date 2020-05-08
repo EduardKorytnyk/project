4bc9ed1011706eafb1c288e4b2b3482ad539b0bf
Eduard <edikko444@gmail.com>
2020-05-08 12:29:34 +0300

    part of the cycle

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let money = prompt("Ваш бюджет на месяц?", 0);
+let money = +prompt("Ваш бюджет на месяц?", 0);
 let time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
 let appData = {
     budget: money,
@@ -9,21 +9,30 @@ let appData = {
     savings : false
 };
 
-let sum = prompt("Введите обязательную статью расходов в этом месяце", 0);
-let allsum = prompt("Во сколько обойдется?", );
 
-if (sum){
- appData.expenses.sum = allsum 
- 
+
+for (let i=0; i<2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", 0),
+        b = prompt("Во сколько обойдется?", );
+        if ((typeof(a))==="string"&&(typeof(a)) != null && (typeof(b)) !=null 
+        && a != '' && b != '' && a.length<50 ){
+           console.log("done");
+            appData.expenses[a] = b;
+        } else {
+            //бисать else и вернуться к вопросу обратно 
+
+        }
+};
+
+appData.moneyPerDay = appData.budget / 38; 
+alert("Ежедневный бюджет: "+ appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100){
+    console.log("Минимальный уроовень достатка")
+}else if (appData.moneyPerDay >100 && appData.moneyPerDay < 2000){
+    console.log("Средний уроовень достатка")
+}else if (appData.moneyPerDay > 2000 ){ 
+     console.log("Высокий уроовень достатка")
+}else {
+    console.log("Ошибка")
 }
-
-console.log(appData)
-console.log(appData.expenses)
-
-//expenses.sum = allsum;
-console.log(appData.budget)
-console.log(appData.timeData)
-
-let monthBudget = appData.budget/30
-alert("ваш дневной бюджет " + monthBudget);
-
